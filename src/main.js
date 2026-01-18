a0ad4835c7bb4df8c07366d6587d1fdc4eca9cc2
Tiago Schwab <tiagoschwabbs@gmail.com>
2026-01-18 16:27:36 -0300

    Remover horários fixos do HTML e adicionar arquivos de script para manipulação de formulário e carregamento

--- src/main.js
+++ src/main.js
@@ -5,3 +5,6 @@ import "./assets/libs/day.js"
 import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
+
+import "./modules/form/submit.js"
+import "./modules/loads.js"
